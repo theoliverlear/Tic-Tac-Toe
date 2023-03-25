5dfd5a42c3c14836c8f94083d6935b8781f2ebe6
theoliverlear <sigwartholiver2@gmail.com>
2023-03-24 19:15:11 -0500

    Full Project v1.0
    Updated syntax, styling, and boolean logic.

---
+++ cmake-build-debug/CMakeFiles/ttt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ttt.dir/main.cpp.obj"
+  "CMakeFiles/ttt.dir/main.cpp.obj.d"
+  "libttt.dll.a"
+  "ttt.exe"
+  "ttt.exe.manifest"
+  "ttt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ttt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Owner/CLionProjects/ttt
+# Install script for directory: C:/Users/olive/GitHub/Tic-Tac-Toe
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/olive/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/223.8836.42/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Owner/CLionProjects/ttt/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/olive/GitHub/Tic-Tac-Toe/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
